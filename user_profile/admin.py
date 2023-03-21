736130033feaae7b1ea616d76aaf7058419748f7
Kamil Skubis <kamilskubiss@hotmail.com>
2023-03-21 13:39:51 +0100

    add child to admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile
-
+from .models import Profile, Child, Task
 
 admin.site.register(Profile)
+admin.site.register(Child)
+admin.site.register(Task)
