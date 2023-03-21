08bcd3bf9b5f1c82fe3988995a5c7a53bedf0571
Kamil Skubis <kamilskubiss@hotmail.com>
2023-03-21 13:40:26 +0100

    child model and task model added

--- user_profile/models.py
+++ user_profile/models.py
@@ -3,6 +3,15 @@ from django.db import models
 from PIL import Image
 
 
+class Child(models.Model):
+    name = models.CharField(max_length=200)
+    date_of_birth = models.DateField(max_length=8)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(
@@ -11,6 +20,7 @@ class Profile(models.Model):
     )
     bio = models.TextField()
     age = models.PositiveIntegerField(null=True, blank=True)
+    children = models.ManyToManyField(Child, blank=True)
 
     def __str__(self):
         return f'{self.user} Profile'
@@ -23,3 +33,17 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.avatar.path)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(max_length=8)
+    children = models.ManyToManyField(Child)
+
+    def __str__(self):
+        return f"{self.title}: due {self.due_date}"
+
+    class Meta:
+        ordering = ["due_date"]
